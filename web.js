5b5d159b481bf10e51ddf5073e532944365dd93d
Ubuntu <ubuntu@ip-10-164-111-85.ec2.internal>
2013-07-24 05:18:44 +0000

    configured web.js to serve static files

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   var responseString = fs.readFileSync("index.html", encoding='utf8');
   response.send(responseString);
